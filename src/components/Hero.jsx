9d66052f21c713c2b8ab65ed84ab5f5e2c743f83
MyName <tejuchalla017@gmail.com>
2025-07-13 18:57:43 +0100

    HeroGifsUpdate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 function Hero() {
   return (
     <section
-      className="relative h-96 flex flex-col justify-center items-center text-white text-center bg-cover bg-center bg-no-repeat"
+      className="relative h-[300px] md:h-[300px] lg:h-[625px] flex flex-col justify-center items-center text-white text-center bg-cover bg-center bg-no-repeat"
       style={{
-        backgroundImage: 'url(/images/hero2.jpg)',
+        backgroundImage: 'url(/images/skills/FullStack.gif)',
+        backgroundSize: '100%',
       }}
     >
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black opacity-75"></div>
+      <div className="absolute inset-0 bg-black opacity-85"></div>
 
       {/* Fixed Navbar */}
       <div className="fixed top-0 left-0 w-full flex justify-between items-center p-4 bg-black bg-opacity-50 text-white z-50">
@@ -28,8 +29,15 @@ function Hero() {
       </div>
 
       {/* Center Content */}
-      <div className="relative z-10 mt-20">
-        <p className="text-2xl mt-4">Python Full Stack Developer | London, UK</p>
+      <div className="relative z-10 mt-20 text-center">
+        <p className="text-2xl mb-6">Python Full Stack Developer</p>
+
+        {/* Info Box */}
+        <div className="bg-white bg-opacity-5 backdrop-blur-sm p-6 rounded-2xl shadow-lg max-w-[800px] mx-auto text-white">
+          <p className="text-lg">
+            I'm a Python Full Stack Developer with experience building secure, scalable, and high-performance web applications. I work with Django and Flask for backend development, and use React.js to build fast, responsive user interfaces. I also design and work with RESTful APIs, connecting frontend and backend into smooth, full-featured applications. I have practical experience in deploying applications and managing cloud infrastructure with Microsoft Azure. I focus on building reliable systems that are ready for real-world use and easy to scale.
+          </p>
+        </div>
       </div>
     </section>
   );
